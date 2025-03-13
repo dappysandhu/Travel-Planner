100d6ed0b9379e3cd546f4dcbb249a31bc25b6bf
Dilpreet Sandhu <dappysandhu1@gmail.com>
2025-03-13 16:36:12 -0700

    Revert "Develop"

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import "./App.css";
+import Auth from "./pages/Auth";
 
 function App() {
   return (
     <div className="app-container">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          {/* Add routes for Login and Register */}
-          <Route path="/login" element={<div>Login Page</div>} />
-          <Route path="/register" element={<div>Register Page</div>} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </Router>
     </div>
